5c369ca7cbc32d10126df9adc80c418a878b208d
amarrero86mr <amarrero86mr@gmail.com>
2025-12-08 14:25:29 -0300

    agregando index.html y acceso desde index.js a paginas estaticas/publicas

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 import express from 'express';
-import { db } from "./src/db/firebase-db.config.js";
 import cors from "cors";
 import PRODUCT_ROUTER from './src/routes/products.routes.js';
+import path from "path";
+import { fileURLToPath } from 'url';
 
 const app = express()
 const port = 3000
 
-// const corsConfig = {
-//     origin: ['http://localhost:3000', 'https://midominio.com'],
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     exposedHeaders: ['Content-Length'],
-//     credentials: true,
-//     maxAge: 600,
-//     optionsSuccessStatus: 204
-// }
+const __fileName = fileURLToPath(import.meta.url)
+const __dirname = path.dirname( __fileName );
+console.log("__fileName: ", __dirname)
 
-// app.use(cors(corsConfig))
+/* const corsConfig = {
+    origin: ['http://localhost:3000', 'https://midominio.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Length'],
+    credentials: true,
+    maxAge: 600,
+    optionsSuccessStatus: 204
+} */
+
+//app.use(cors(corsConfig))
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,"src/public/")));
 
-app.get('/', (req, res) => {
-  res.send("Hola vengo a flotar")
-})
+app.use(express.static("public"));
+app.get('/', (req, res) => { res.redirect("index") })
 
+// Rutas protegidas
 app.use("/api", PRODUCT_ROUTER)
 
 app.listen(port, () => {
