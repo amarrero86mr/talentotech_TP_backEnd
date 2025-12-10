b872094355f63908a7d460cd458bd4d438e06716
amarrero86mr <amarrero86mr@gmail.com>
2025-12-10 20:44:54 -0300

    agregando metodo de autenticacion

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import PRODUCT_ROUTER from './src/routes/products.routes.js';
 import path from "path";
 import { fileURLToPath } from 'url';
 import VISITORS_ROUTER from './src/routes/visitors.routes.js';
+import { requireAuth } from './src/middleware/authentication.js';
 
 const app = express()
 const port = 3000
@@ -30,9 +31,17 @@ app.use(express.static(path.join(__dirname,"src/public/")));
 app.use(express.static("public"));
 app.get('/', (req, res) => { res.redirect("index") })
 
-// Rutas protegidas
-app.use("/api", PRODUCT_ROUTER);
 app.use("/api/visitors", VISITORS_ROUTER);
+
+// Rutas protegidas
+
+app.use(requireAuth);
+
+app.use((req, res, next) => {
+  console.log(`Datos received at:  ${req.method} ${req.url}`);
+  next();
+});
+app.use("/api", PRODUCT_ROUTER);
 
 app.listen(port, () => {
   console.log(`Server corriendo en Puerto: localhost:${port}`)
---
+++ src/middleware/authentication.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
+
+export const requireAuth = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader?.split(" ")[1]; // formato "Bearer <token>"
+
+    if (!token) {
+        return res.status(403).json("Access denied. Token missing.");
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || "default_secret");
+        req.visitor = decoded; // guarda los datos del visitor autenticado
+        next();
+    } catch (err) {
+        return res.status(err.status).json("Invalid or expired token.", err);
+    }
+};
